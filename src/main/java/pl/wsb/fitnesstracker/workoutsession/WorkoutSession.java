6b8dbf01cac3a7984dc8b5f8a5f7a3411faad3ed
Nicek <julia10023gmail.com>
2026-05-09 13:54:56 +0200

    Implement LAB03 JPA entities and repositories

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,14 +1,36 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import pl.wsb.fitnesstracker.training.api.Training;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "workout_session")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WorkoutSession {
 
     @Id
-    private int id;
-    private int trainingId;
-    private String timestamp;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "training_id", nullable = false)
+    private Training training;
+
+    private LocalDateTime timestamp;
     private double startLatitude;
     private double startLongitude;
     private double endLatitude;
